da0e4aabbfbcaef262a1523e4418c99eba292c36
Eigil Mandrup Nikolajsen <eigi0088@gmail.com>
2022-06-07 17:35:28 +0200

    added svg and css layout for loading and splash

---
+++ js/kendditkryb.js
@@ -0,0 +1,30 @@
+let kdk = {
+    game: {
+        categories: {
+            biller: {
+                levels: {
+                    1: {
+                        level: 1,
+                        difficulty: "easy",
+                        unlocked: true,
+                        playable: true,
+                        completed: 3,
+                        insect: "Syvplettet mariehøne",
+                        anagram: "syv-plettet_marie-høne",
+                        hint1: "#syv#-plettet_marie-høne",
+                        hint2: "#syv-plettet#_marie-høne",
+                        wincontent: {
+                            beskrivelse: "Et af de mest velkendte insekter. En oval, stærkt hvælvet bille med røde dækvinger og syv sorte pletter. Hovedet og forkroppen er sort med hvide pletter. Den røde farve fungerer som advarselsfarve, der fortæller eventuelle fjender, at de bør holde sig væk pga. mariehønens dårlige lugt og smag. Længde 7-10 mm.",
+                            img1: "Mariehøne på blad",
+                            forvekslingsmuligheder: "Ligner overfladisk flere andre arter af mariehøns, men kan adskilles fra disse på antallet af sorte pletter og de hvide tegninger på hoved og forkrop.",
+                            biologi: "Lever overvejende af bladlus og i mindre grad af andre insekter samt pollen og svampesporer. Træffes fra det tidlige forår til sent efterår - af og til også på lune vinterdage. Overvintrer som voksen.",
+                            img2: "Mariehøne i haven",
+                            levested: "Forekommer stort set overalt, hvor der er gode bestande af byttedyr, primært bladlus, i græsland, agerland, haver og parker samt på enge.",
+                            udbredelse: "Udbredt og almindelig over hele landet. I år med mange bladlus kan den optræde i enorme mængder, hvilket bl.a. kan ses langs kysterne, hvor den kan skylle op i bunkevis.",
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
