a4fa860df35e0612f4b4993cf51c7ad1a4ce9f60
Eigil Mandrup Nikolajsen <eigi0088@gmail.com>
2022-06-08 13:34:12 +0200

    playing with draggable

--- js/index.js
+++ js/index.js
@@ -34,3 +34,42 @@ fetch('img/mask/mask1.svg')
         document.querySelector("#game_img_container").innerHTML = text
     })
     .catch(console.error.bind(console));
+
+const letterSizeRecalc = () => {
+    document.querySelectorAll("#game_letter_grid span.letter").forEach((el) => {
+        el.style.width = `100%`
+        setTimeout(() => {
+            let w = el.offsetWidth
+            el.style.padding = `${w * 0.55 - 3}px 0 ${w * 0.45 - 3}px 0`
+            el.style.width = `${w}px`
+        }, 50)
+    })
+}
+
+letterSizeRecalc()
+
+window.addEventListener("resize", () => {
+    letterSizeRecalc()
+})
+
+const sortable = new Draggable.Sortable(document.querySelectorAll('#game_letter_grid'), {
+    draggable: '#game_letter_grid span',
+    droppable: '#game_letter_grid span.dragge',
+    handle: '#game_letter_grid span.dragge',
+    plugins: [Draggable.Plugins.SortAnimation],
+    sortAnimation: {
+        duration: 200,
+        easingFunction: 'ease-out',
+    },
+    mirror: {
+
+    }
+})
+
+sortable.on('drag:start', (el) => {
+    console.log(el.data.source)
+});
+
+sortable.on('drag:sorted', (el) => {
+    console.log(el)
+});
